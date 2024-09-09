36632f5ffa64d437eccd3b970edf83c8fd89af1d
Gagou1 <valentingarino@gmail.com>
2024-09-09 16:07:20 +0200

    Add files via upload

---
+++ Packages/ModelClasses.py
@@ -0,0 +1,93 @@
+import numpy as np
+import math
+import random
+
+
+
+class FellerErrorException(Exception):
+    #Raise this exception if the Feller condition of the Heston model is not satisfied
+    pass
+
+
+
+class BlackScholesModel(object):
+
+#dS_t=r*S_t*dt+sigma*S_t*dW_t
+
+    def __init__(self, r, sigma, S0):
+        self.r=r
+        self.sigma=sigma
+        self.S0=S0
+       
+        
+    def getRate(self):
+        return self.r
+    
+        
+        
+    def createPath(self,steps,T):
+        BScDiffusion=np.zeros(steps+1, dtype=float)
+        BScDiffusion[0]=self.S0
+        Vol=self.sigma*np.ones(steps+1,dtype=float)
+        
+        for j in range(1,steps+1):
+            BScDiffusion[j]=(BScDiffusion[j-1]+BScDiffusion[j-1]*self.r*(T/steps)
+                              +BScDiffusion[j-1]*self.sigma*(math.sqrt(T)*np.random.normal()/math.sqrt(steps)))
+        return [BScDiffusion,vol]
+    
+
+
+
+class HestonModel(object):
+
+#dS_t=r*S_t*dt+sqrt(v_t)*S_t*dW^1_t
+#dv_t=kappa*(vb-v_t)*dt+epsilon*sqrt(v_t)*(rho*dW^1_t+sqrt(1-rho^2)dW^2_t)
+
+    def __init__(self, r, kappa, v0, vb, epsilon, rho, S0):
+        self.r=r
+        self.kappa=kappa
+        self.v0=v0
+        self.vb=vb
+        self.epsilon=epsilon
+        self.rho=rho
+        self.S0=S0
+        
+        try:
+            if 2*self.kappa*self.vb<=self.epsilon**2:
+                raise FellerErrorException
+        except FellerErrorException:
+            print('The Feller condition is not satisfied')
+            
+        
+    def getRate(self):
+        return self.r
+    
+    
+       
+    def __correlatedBrownian(self,steps):
+        
+        B1=np.random.normal(0,1,steps)
+        W=np.random.normal(0,1,steps)
+        B2=(self.rho)*B1+(np.sqrt(1-(self.rho)**2))*W
+        return(B1,B2)
+       
+        
+    def createPath(self,steps,T):
+        HestDiffusion=np.zeros(steps+1, dtype=float)
+        HestDiffusion[0]=self.S0
+        Vol=np.zeros(steps+1, dtype=float)
+        Vol[0]=self.v0
+        B1=self.__correlatedBrownian(steps)[0]
+        B2=self.__correlatedBrownian(steps)[1]
+        #Generate the volatility
+        
+        for j in range(1,steps+1):
+            Vol[j]=(Vol[j-1]+self.kappa*(self.vb-Vol[j-1])*(T/steps)
+                    +self.epsilon*math.sqrt(max(Vol[j-1],0))*math.sqrt(T)/math.sqrt(steps)*B2[j-1])
+        #Generate the stock price
+        
+        for j in range(1,steps+1):
+            HestDiffusion[j]=(HestDiffusion[j-1]+self.r*HestDiffusion[j-1]*(T/steps)
+                             +math.sqrt(max(Vol[j-1],0))*HestDiffusion[j-1]*math.sqrt(T)/math.sqrt(steps)*B1[j-1]) 
+        return [HestDiffusion,Vol]
+        
